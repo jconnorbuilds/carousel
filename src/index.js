f506f52aefc835a0b1208c219bc2f312ade41855
Joe Connor <joseph.connor.music@gmail.com>
2024-05-02 21:10:51 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './index.css';
+console.log('hey there world');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['./src/**/*.html'],
+    hot: true,
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }],
+  },
+};
